1000126daea3251fdd86473284a4ebe3ea9488fd
SO-HUII <haejuuun925@gmail.com>
2023-05-02 13:27:41 +0900

    [문제 4] 과목의 총점과 평균을 정수 형태로 출력
    총점과 평균을 int로 강제 형변환하여 해결

--- src/main/java/com/ohgiraffers/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -3,7 +3,7 @@ package com.ohgiraffers.level02.normal;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
 		/* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
 		 * 총점과 평균을 정수 형태로 출력하세요
 		 * 
@@ -11,7 +11,16 @@ public class Application2 {
 		 * 총점 : 201
 		 * 평균 : 67
 		 *  */
-		
+
+		float korean = 80.5f;
+		float math = 50.6f;
+		float english = 70.8f;
+
+		double sum = korean + math + english;
+		double avg = sum / 3;
+
+		System.out.println("총점 : " + (int) sum);
+		System.out.println("평균 : " + (int) avg);
 	}
 
 }
