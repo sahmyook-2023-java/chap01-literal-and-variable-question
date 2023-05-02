7d81e450a5b38525116c2f6093b58d4af92e5d84
DESKTOP-OJVF8KK\seol <tjfrhkdwls12@naver.com>
2023-05-02 13:16:12 +0900

    3번 문제 넓이와 둘레 구하기

--- src/main/java/com/ohgiraffers/level01/basic/Application2.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application2.java
@@ -3,7 +3,10 @@ package com.ohgiraffers.level01.basic;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
+		double width = 12.5, height = 36.4;
+		System.out.println("면적 : "+ (width*height));
+		System.out.println("둘레 : "+ 2*(width+height));
 		/* 사각형의 넓이와 둘레를 구하는 문제이다.
 		 * 너비 12.5 높이 36.4를 변수에 저장하고
 		 * 각 넓이와 둘레를 계산해 변수에 담고 아래와 같이 출력되도록 하시오
@@ -12,7 +15,7 @@ public class Application2 {
 		 * 면적 : 455.0
 		 * 둘레 : 97.8
 		 *  */
-		
+
 	}
 
 }
