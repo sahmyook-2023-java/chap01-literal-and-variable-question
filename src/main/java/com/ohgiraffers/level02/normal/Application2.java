af552a4b22cda87011f8744e655aec0dd2b882b9
jo <jojoo9279@gmail.com>
2023-05-02 13:55:04 +0900

    문제 해결

--- src/main/java/com/ohgiraffers/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -3,7 +3,16 @@ package com.ohgiraffers.level02.normal;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+		double kor = 80.5;
+		double math = 50.6;
+		double eng = 70.8;
+
+		int sum = 0;
+		int avg = 0;
+		sum += kor + math + eng;
+		avg += (kor+math+eng)/3;
+		System.out.println("총점 : " + sum);
+		System.out.println("평균 : " + avg);
 		/* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
 		 * 총점과 평균을 정수 형태로 출력하세요
 		 * 
