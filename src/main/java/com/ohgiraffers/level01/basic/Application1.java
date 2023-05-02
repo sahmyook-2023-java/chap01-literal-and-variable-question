57e942d2846b3b28fbb51b7edb2b4370face83f7
jongkyuh <jongkyuh@naver.com>
2023-05-02 13:13:54 +0900

    [문제1] 사칙연산 출력 문제
    사칙연산 출력 문제

--- src/main/java/com/ohgiraffers/level01/basic/Application1.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application1.java
@@ -14,7 +14,15 @@ public class Application1 {
 		 * 나누기한 몫 : 0
 		 * 나누기한 나머지 : 20
 		 * */
-		System.out.println("함종규 풀리퀘스트 테스트 두번째");
+		int a = 20;
+		int b = 30;
+
+		System.out.println("더하기 결과 : " + (20+30));
+		System.out.println("빼기 결과 : " + (20-30));
+		System.out.println("곱하기 결과 : " + (20*30));
+		System.out.println("나누기한 몫 : " + (20/30));
+		System.out.println("나누기한 나머지 : " + (20%30));
+
 	}
 
 }
