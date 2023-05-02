7d2b943c5cc2fc6160aad98096c01605d351a373
DESKTOP-67GVLQD\seongmin <dltgld72@naver.com>
2023-05-02 13:14:52 +0900

    [문제2] 사칙연산 출력 문제 해결
    사칙연산 출력 문제(문제 2번)을 풀이 완료

--- src/main/java/com/ohgiraffers/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -3,7 +3,7 @@ package com.ohgiraffers.level02.normal;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
 		/* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
 		 * 총점과 평균을 정수 형태로 출력하세요
 		 * 
@@ -11,7 +11,12 @@ public class Application2 {
 		 * 총점 : 201
 		 * 평균 : 67
 		 *  */
-		
+		float koreanGrade = 80.5f;
+		float mathGrade = 50.6f;
+		float englishGrade = 70.8f;
+		int result = (int) (koreanGrade + mathGrade + englishGrade);
+		System.out.println("총점 : " + result);
+		System.out.println("평균 : " + result/3);
 	}
 
 }
