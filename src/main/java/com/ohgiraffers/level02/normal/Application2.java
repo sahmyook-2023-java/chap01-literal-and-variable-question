77a90f3398d77e1aa98ebab319e213bdcfd7c51a
DESKTOP-OJVF8KK\seol <tjfrhkdwls12@naver.com>
2023-05-02 13:16:48 +0900

    4번 문제 총점과 평균 구하기 형 변환에 대해 이해했다.

--- src/main/java/com/ohgiraffers/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -3,7 +3,10 @@ package com.ohgiraffers.level02.normal;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+		double ko = 80.5, math = 50.6, en = 70.8;
+		System.out.println("총점: " + (int)(ko+math+en));
+		System.out.println("평균: " + (int)((ko+math+en)/3));
+
 		/* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
 		 * 총점과 평균을 정수 형태로 출력하세요
 		 * 
@@ -11,7 +14,7 @@ public class Application2 {
 		 * 총점 : 201
 		 * 평균 : 67
 		 *  */
-		
+
 	}
 
 }
